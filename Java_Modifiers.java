a7b45ef84da786d4ec922c0529487b021e028302
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2023-11-20 00:17:13 -0800

    update

---
+++ Java_Modifiers.java
@@ -0,0 +1,40 @@
+// 1- final 
+// If you don't want the ability to override existing attribute values,
+// declare attributes as final
+public class Main {
+  final int x = 10;
+  final double PI = 3.14;
+
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    myObj.x = 50; // will generate an error: cannot assign a value to a final variable
+    myObj.PI = 25; // will generate an error: cannot assign a value to a final variable
+    System.out.println(myObj.x);
+  }
+}
+
+// 2- static 
+
+// static method means that it can be accessed without creating an object of the class,
+// unlike public
+
+public class Main {
+  // Static method
+  static void myStaticMethod() {
+    System.out.println("Static methods can be called without creating objects");
+  }
+
+  // Public method
+  public void myPublicMethod() {
+    System.out.println("Public methods must be called by creating objects");
+  }
+
+  // Main method
+  public static void main(String[ ] args) {
+    myStaticMethod(); // Call the static method
+    // myPublicMethod(); This would output an error
+
+    Main myObj = new Main(); // Create an object of Main
+    myObj.myPublicMethod(); // Call the public method
+  }
+}
